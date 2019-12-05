fd3813877135492d7360d0bfe7bb7d73b8a012a9
meidina-hashmicro <57191706+meidina-hashmicro@users.noreply.github.com>
2019-12-05 09:34:34 +0700

    revision customer_product_sku_mapping

--- customer_product_sku_mapping/models/sku.py
+++ customer_product_sku_mapping/models/sku.py
@@ -94,10 +94,37 @@ class PartnerSku(models.Model):
     price = fields.Float(string="Price", required=True)
 
 
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.multi
+    @api.onchange('partner_id')
+    def onchange_partner_id(self):
+        res = super(SaleOrder, self).onchange_partner_id()
+        if len(self.partner_id.partner_sku_ids) > 0:
+            product_list = [pro for pro in self.partner_id.partner_sku_ids.product_id]
+            data_list = []
+            for pro_sku in product_list:
+                name = pro_sku.name_get()[0][1]
+                if pro_sku.description_sale:
+                    name += '\n' + pro_sku.description_sale
+                data_list.append({
+                    'product_id': pro_sku.id,
+                    'sku_id': self.partner_id.partner_sku_ids.sku_id.id,
+                    'name': name,
+                    'product_uom': pro_sku.uom_id,
+                    'product_uom_qty':  1.0
+                    })
+            self.order_line = [(0, 0, d) for d in data_list]
+        # else:
+            # self.order_line = []
+        return res
+
+
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    sku_id = fields.Many2one('sku.sku', string="No.SKU")
+    sku_id = fields.Many2one('sku.sku', string="Customer SKU")
 
     @api.multi
     @api.onchange('product_id')
@@ -111,28 +138,35 @@ class SaleOrderLine(models.Model):
             self.sku_id = sku_id.id
         return res
 
-
-# class ProductProduct(models.Model):
-#     _inherit = 'product.product'
-
-#     @api.model
-#     def name_search(self, name, args=None, operator='ilike', limit=100):
-#         args = args or []
-#         domain = []
-#         if name:
-#             domain = ['|', ('code', '=ilike', name + '%'), ('name', operator, name)]
-#             if operator in expression.NEGATIVE_TERM_OPERATORS:
-#                 domain = ['&', '!'] + domain[1:]
-#         products = self.search(domain + args, limit=limit)
-#         if self._context.get('partner_id'):
-#             partner_id = self._context.get('partner_id')
-#             sku_obj = self.env['sku.sku'].search([('customer', '=', partner_id)])
-#             products_res = sku_obj.sku_product_info_ids.mapped('product_id')
-#             if any(products_res):
-#                 products = products_res
-#             else:
-#                 return products_res.name_get()
-#         return products.name_get()
+    # @api.model
+    # def _search(self, args, offset=0, limit=None, order=None, count=False, access_rights_uid=None):
+    #     categ_id = self.env.context.get('id', False)
+    #     print "ffffffff", self, categ_id
+    #     child_id = self.env['sale.order'].browse(categ_id)
+    #     print "qqqqqqqqqqqq", child_id
+    #     customer_id = self.order_id.partner_id
+    #     print ('aaaaaaaaa', customer_id)
+    #     if customer_id:
+    #         # sku_id = self.env['sku.sku'].serach([('customer', '=', customer_id.id)])
+    #         # if sku_id:
+    #         self._cr.execute("""
+    #             SELECT
+    #                 product_id
+    #             FROM
+    #                 sku_product_info
+    #             WHERE
+    #                 sku_id in ( SELECT id FROM sku_sku where customer = (%s)
+    #                             )
+    #         """ % (customer_id.id))
+    #         res = self._cr.dictfetchall()
+    #         print ("ssssssssssss", res)
+    #         product_id = []
+    #         if res:
+    #             product_id = [i['product_id'] for i in res]
+    #         args += [('id', 'in', product_id)]
+    #     return super(SaleOrderLine, self)._search(args=args, offset=offset,
+    #                                               limit=limit, order=order,
+    #                                               count=count, access_rights_uid=access_rights_uid)
 
 
 class ProductTemplate(models.Model):
