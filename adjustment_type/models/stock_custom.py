460dc0efa7de797f95bed6715d5070f7d67c9699
meidina-hashmicro <57191706+meidina-hashmicro@users.noreply.github.com>
2019-12-05 13:25:18 +0700

    add adjustment type

--- adjustment_type/models/stock_custom.py
+++ adjustment_type/models/stock_custom.py
@@ -9,3 +9,10 @@ class adjustment_type(models.Model):
     code =fields.Text("Code")
     description = fields.Text("Description")
     action = fields.Text("Action")
+    adjustment_type = fields.Char("Adjustment type")
+
+
+class StockInventory(models.Model):
+    _inherit = 'stock.inventory'
+
+    adjustment_type = fields.Many2one(comodel_name="adjustment.type", string="Adjustment type")
