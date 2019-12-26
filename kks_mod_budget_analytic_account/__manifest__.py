f84297d26e42f2405846bc220bbd6947b31d1d4e
meidina-hashmicro <57191706+meidina-hashmicro@users.noreply.github.com>
2019-12-26 17:42:34 +0700

    add kks_mod_budget_analytic_account

---
+++ kks_mod_budget_analytic_account/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Analytic Account Mandatory",
+    "author": "HashMicro/ Amit Patel",
+    "version": "10.0.1.0",
+    "website": "www.hashmicro.com",
+    "category": "Budget",
+    'summary': 'Analytic Account Mandatory',
+    'description': """
+        This module help to make Analytic Account mandatory field in Budget Lines.
+    """,
+    "depends": ["account_budget"],
+    "data": [
+        "views/view.xml",
+    ],
+    'demo': [],
+    "installable": True,
+    "auto_install": False,
+    "application": True,
+}
